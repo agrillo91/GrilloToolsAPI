526ccd2cdec042ac8dc9ea49a42e7cf42e7baa0a
Alberto G <58365565+agrillo91@users.noreply.github.com>
2025-10-07 18:22:42 +0200

    allowed Host update

--- grillotoolsapi/settings.py
+++ grillotoolsapi/settings.py
@@ -32,7 +32,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['grillotoolsapi.onrender.com']
 
 
 # Application definition
