c1867a7348324658fa225f1e216c36356b0adf92
Alberto G <58365565+agrillo91@users.noreply.github.com>
2025-10-08 16:17:27 +0200

    Update settings.py

--- grillotoolsapi/settings.py
+++ grillotoolsapi/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # URL desde la que se servirán
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
 # Default primary key field type
