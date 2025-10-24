ec91b9ca51266368761a2545f254d4bd31784a04
Alberto G <58365565+agrillo91@users.noreply.github.com>
2025-10-24 12:28:35 +0200

    urls validated

--- licenseManager/urls.py
+++ licenseManager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import verify_license
+from .views import verify_license, validate_token
 
 urlpatterns = [
     path('verify/', verify_license, name='verify_license'),
+    path("validate/", validate_token, name="validate_token"),  # 👈 Añádela
 ]
+
